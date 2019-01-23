942d3e8ab89569be3a4261eaadba869a6102ee1b
Shawn Simon <ShawnSimon@Shawns-MacBook-Pro.local>
2019-01-22 22:38:42 -0600

    Should be done; can debug

--- crc.c
+++ crc.c
@@ -21,6 +21,43 @@ int connect_to(const char *host, const int port);
 struct Reply process_command(const int sockfd, char* command);
 void process_chatmode(const char* host, const int port);
 
+void* recv_msg_handler(void* cr_fd) {
+    int chatroom_fd =  *((int *)cr_fd);
+    char receiveMessage[201] = {};
+    int one = 1;
+    while (one != 0) {
+        int receive = recv(chatroom_fd, receiveMessage, 201, 0);
+        int message_len = strlen(receiveMessage);
+
+        //printf("length: %d\n",message_len);
+        //printf("AFTER\n");
+        if (message_len > 0) {
+            display_message(receiveMessage);
+        } else if (message_len == 0) {
+            continue;
+        } else { 
+            // -1 
+        }
+        //printf("one: %d\n", one);
+    }
+}
+
+void* send_msg_handler(void* cr_fd) {
+    int chatroom_fd =  *((int *)cr_fd);
+    char message[101] = {};
+    while (1) {
+        get_message(message, 101);
+
+        // printf("\r%s", "> ");
+        // fflush(stdout);
+
+        send(chatroom_fd, message, 101, 0);
+        if (strcmp(message, "exit") == 0) {
+            break;
+        }
+    }
+}
+
 int main(int argc, char** argv)
 {
   if (argc != 3) {
@@ -128,6 +165,66 @@ struct Reply process_command(const int sockfd, char* command)
   //
   // - CREATE/DELETE/JOIN and "<name>" are separated by one space.
   // ------------------------------------------------------------
+    char delim[] = " ";
+    int inputLength = strlen(command);
+    char *temp = (char*) calloc(inputLength + 1, sizeof(char));
+    strncpy (temp,command, inputLength);
+    char* ptr = strtok(temp, delim);
+    char buf[1000];
+    int numbytes = 0;
+
+
+    if(strcmp(ptr, "CREATE") == 0){
+      send(sockfd, command, inputLength, 0);
+      numbytes = recv(sockfd, buf, 999, 0);
+      //printf("%s\n", buf);
+
+      struct Reply reply; 
+      //reply.status = 
+      return reply;
+    } 
+    else if(strcmp(ptr, "DELETE") == 0){
+      send(sockfd, command, inputLength, 0);
+      numbytes = recv(sockfd, buf, 999, 0);
+      //printf("%s\n", buf);      
+
+      // WHAT TO DO 
+    } 
+    else if(strcmp(ptr, "JOIN") == 0){
+      send(sockfd, command, inputLength, 0);
+      numbytes = recv(sockfd, buf, 999, 0);
+      //printf("%s\n", buf);
+
+      int buflength = strlen(buf);
+      char *copy_buf = (char*) calloc(inputLength + 1, sizeof(char));
+      strncpy (copy_buf, buf, buflength);
+      char* token = strtok(copy_buf, delim);
+      int port_num = atoi(token);
+      token = strtok(NULL, delim);
+      int member_num = atoi(token);
+
+
+
+      struct Reply reply;
+      //reply.status = 
+      reply.num_member = member_num; 
+      reply.port = port_num; 
+
+      return reply;
+    } 
+    else if(strcmp(ptr, "LIST") == 0){
+      send(sockfd, command, inputLength, 0);
+      numbytes = recv(sockfd, buf, 999, 0);
+      //printf("%s\n", buf);
+
+      struct Reply reply; 
+      //reply.status = 
+      strcpy(reply.list_room, buf);
+      return reply;
+    } 
+    else{
+      printf("Command not recognized");
+    }
 
 
   // ------------------------------------------------------------
@@ -180,11 +277,6 @@ struct Reply process_command(const int sockfd, char* command)
   // ------------------------------------------------------------
 
   // REMOVE below code and write your own Reply.
-  struct Reply reply;
-  reply.status = SUCCESS;
-  reply.num_member = 5;
-  reply.port = 1024;
-  return reply;
 }
 
 /*
@@ -201,7 +293,7 @@ void process_chatmode(const char* host, const int port)
   // to the server using host and port.
   // You may re-use the function "connect_to".
   // ------------------------------------------------------------
-
+  int chatroom_fd = connect_to(host, port);
   // ------------------------------------------------------------
   // GUIDE 2:
   // Once the client have been connected to the server, we need
@@ -209,19 +301,39 @@ void process_chatmode(const char* host, const int port)
   // At the same time, the client should wait for a message from
   // the server.
   // ------------------------------------------------------------
+  pthread_t send_msg_thread;
+  pthread_t recv_msg_thread;
 
-    // ------------------------------------------------------------
-    // IMPORTANT NOTICE:
-    // 1. To get a message from a user, you should use a function
-    // "void get_message(char*, int);" in the interface.h file
-    //
-    // 2. To print the messages from other members, you should use
-    // the function "void display_message(char*)" in the interface.h
-    //
-    // 3. Once a user entered to one of chatrooms, there is no way
-    //    to command mode where the user  enter other commands
-    //    such as CREATE,DELETE,LIST.
-    //    Don't have to worry about this situation, and you can
-    //    terminate the client program by pressing CTRL-C (SIGINT)
+  while(1){
+    //printf("BEFORE JOIN\n");
+  
+    (void) pthread_join(send_msg_thread, NULL);
+    (void) pthread_join(recv_msg_thread, NULL);
+    
+    //printf("BEFORE SEND\n");
+    if (pthread_create(&send_msg_thread, NULL, &send_msg_handler, (void*) &chatroom_fd) != 0) {
+        printf ("Create pthread error!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //printf("BEFORE RECEIVE\n");
+    if (pthread_create(&recv_msg_thread, NULL, &recv_msg_handler, (void*) &chatroom_fd) != 0) {
+        printf ("Create pthread error!\n");
+        exit(EXIT_FAILURE);
+    }
+  }
+  // ------------------------------------------------------------
+  // IMPORTANT NOTICE:
+  // 1. To get a message from a user, you should use a function
+  // "void get_message(char*, int);" in the interface.h file
+  //
+  // 2. To print the messages from other members, you should use
+  // the function "void display_message(char*)" in the interface.h
+  //
+  // 3. Once a user entered to one of chatrooms, there is no way
+  //    to command mode where the user  enter other commands
+  //    such as CREATE,DELETE,LIST.
+  //    Don't have to worry about this situation, and you can
+  //    terminate the client program by pressing CTRL-C (SIGINT)
   // ------------------------------------------------------------
 }
